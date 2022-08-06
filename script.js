9024b1709fffe7461b99610d983a3e284c789d87
mateusz3225 <mateusz3225@gmail.com>
2022-08-06 22:56:24 +0200

    added basic code

---
+++ script.js
@@ -0,0 +1,5 @@
+const displayResult= document.querySelector('.displayResult');
+const add= document.querySelector('.add');
+const subtract= document.querySelector('.subtract');
+const multiply= document.querySelector('.multiply');
+const divide= document.querySelector('.divide');
